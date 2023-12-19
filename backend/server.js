65f48843beeeafcb52bd5e5f63bab02f415f7251
Harsh264209 <hsatpute38@gmail.com>
2023-12-19 18:59:52 +0530

    dotenv file added

--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,35 @@ const express=require('express')
 const bodyParser=require('body-parser')
 const cors=require('cors')
 const app=express()
+require('dotenv').config();
 const mongoose=require('mongoose')
 const userRoutes = require('./Routes/userRoutes');
-const dbConnect=process.env.Db_String || "mongodb://0.0.0.0:27017"
+const dbConnect=process.env.Db_String || "mongodb://localhost:27017"
 const port=process.env.port || 5000
+
+// "mongodb://0.0.0.0:27017"
+
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors())
 
 
-mongoose.connect("mongodb+srv://harsh:harsh45@cluster0.niwycbg.mongodb.net/").then(()=>{console.log("Mongodb connected success")})
+// mongoose.connect(dbConnect).then(()=>{console.log("Mongodb connected success")})
+
+mongoose.connect(dbConnect)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
+
 
 app.get('/',(req,res)=>{
     res.send("Hi I am working properly")
 })
+
 
 
 app.use('/api/user', userRoutes);
